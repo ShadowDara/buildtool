8f66f6a7e107bc4bbdb709190c0434cebbf669ce
ShadowDara <shadowdara74x@gmail.com>
2026-04-10 18:14:35 +0200

    added Info

--- internal/scriptcompiler/path.go
+++ internal/scriptcompiler/path.go
@@ -1,9 +1,17 @@
 package scriptcompiler
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/shadowdara/finder/pub/goansi"
+)
 
 func Addpath() {
-	fmt.Println("export PATH=\"$PWD/.samengine/bin:$PATH")
-	fmt.Println("$env:PATH = \"$PWD\\.samengine\\bin;\" + $env:PATH")
-	fmt.Println("set PATH=%CD%\\.samengine\\bin;%PATH%")
+	fmt.Println(goansi.Green("Add the BIN Path to your Shell\n"))
+	fmt.Println(goansi.Red("# Bash"))
+	fmt.Println("export PATH=\"$PWD/.samengine/bin:$PATH\n")
+	fmt.Println(goansi.Red("# Powershell"))
+	fmt.Println("$env:PATH = \"$PWD\\.samengine\\bin;\" + $env:PATH\n")
+	fmt.Println(goansi.Red("# CMD"))
+	fmt.Println("set PATH=%CD%\\.samengine\\bin;%PATH%\n")
 }
